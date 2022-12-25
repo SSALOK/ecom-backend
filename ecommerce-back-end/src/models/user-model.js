352bd4dea51ff12b968e742d69d9a627f4eeba14
Shubham Alok <alokraj123421@gmail.com>
2022-12-25 23:54:59 +0530

    Revert "ecommerce clone backend first commit"
    This reverts commit 40150c41c1a4d4026a8454a264b18182822619e7.

--- ecommerce-back-end/src/models/user-model.js
+++ ecommerce-back-end/src/models/user-model.js
@@ -1,62 +1,56 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const userSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-      trim: true,
-      min: 3,
-      max: 25,
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const userSchema = new  mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+        min:3,
+        max:25,
     },
-    lastName: {
-      type: String,
-      required: true,
-      trim: true,
-      min: 3,
-      max: 25,
+    lastName:{
+        type:String,
+        required:true,
+        trim:true,
+        min:3,
+        max:25,
     },
-    userName: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true,
-      index: true,
-      lowercase: true,
+    userName:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+        index:true,
+        lowercase:true,
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
+    email:{
+        type: String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
     },
-    hash_password: {
-      type: String,
-      required: true,
+    hash_password:{
+        type: String,
+        required:true,
     },
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
+    role:{
+        type:String,
+        enum:['user', 'admin'],
+        default: 'user',
     },
-    contactNumber: { type: String },
-    profilePicture: { type: String },
-  },
-  { timestamps: true }
-);
+    contactNumber:{type:String},
+    profilePicture:{type:String}
+}, {timestamps:true});
 
-userSchema.virtual("password").set(function (password) {
-  this.hash_password = bcrypt.hashSync(password, 10);
+userSchema.virtual('password')
+.set(function(password){
+    this.hash_password = bcrypt.hashSync(password,10)
 });
+userSchema.method ={
+    authenticate:function(pass){
+        return bcrypt.compareSync(pass, this.hash_password)
+    }
+}
 
-userSchema.virtual("fullname").get(function () {
-  return `${this.firstName}  ${this.lastName} `;
-});
-userSchema.methods = {
-  authenticate: function (pass) {
-    return bcrypt.compareSync(pass, this.hash_password);
-  },
-};
-
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User',userSchema);
