ee9292d4ee1383ac2911f411cb773575a12ac8ab
SSALOK <alokraj123421@gmail.com>
2023-03-07 18:57:39 +0530

    added getcategory

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -25,7 +25,11 @@ exports.getCategories = (req, res) => {
       return res.status(400).json({ error });
     }
     if (categories) {
+<<<<<<< Updated upstream
       res.status(200).json({ categories });
+=======
+      return res.status(200).json({ categories });
+>>>>>>> Stashed changes
     }
   });
 };
--- src/routes/category.js
+++ src/routes/category.js
@@ -4,6 +4,10 @@ const { addCategory, getCategories } = require("../controllers/category");
 const router = express.Router();
 
 router.post("/category/create", addCategory);
+<<<<<<< Updated upstream
 router.get("/category/get", getCategories);
+=======
+router.get("/category/getcategory", getCategories);
+>>>>>>> Stashed changes
 
 module.exports = router;
